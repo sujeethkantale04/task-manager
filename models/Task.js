f10274e9ea8e5a8157601cdc445efe597f22b65f
Kantale Sujeeth <104816459+sujeethkantale04@users.noreply.github.com>
2023-06-27 14:16:09 +0530

    Initial commit

---
+++ models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+// Schema is used we'll setup the structure for all the documents that we'll eventually have in our collection
+const TaskSchema = new mongoose.Schema({
+    // name:String,
+    // completed:Boolean
+
+    name: {
+        type:String,
+        required:[true,'must provide name'],
+        trim: true,
+        maxlength:[20,'name cannot be more than 20 characters'],
+    },
+    completed:{
+        type:Boolean,
+        default: false,
+    },
+})
+
+module.exports = mongoose.model('Task',TaskSchema)
